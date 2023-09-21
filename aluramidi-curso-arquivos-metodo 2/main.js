7ba1f5e57fe83df87919fa20efa4f1bd3fe55682
Lidaiane <129961477+Lidaiane@users.noreply.github.com>
2023-09-21 20:53:47 -0300

    Add files via upload
    método 2 aula 28_09

---
+++ aluramidi-curso-arquivos-metodo 2/main.js
@@ -0,0 +1,33 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play ();    
+}
+
+document.querySelectorAll('.tecla');
+
+const listaDeTeclas= document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick=tocaSomPom;
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play ();
+} 
+listaDeTeclas[1].onclick=tocaSomClap;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play ();
+} 
+listaDeTeclas[2].onclick=tocaSomTim;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play ();
+} 
+listaDeTeclas[3].onclick=tocaSomPuff;
+
+listaDeTeclas[4].onclick=tocaSomSplash;
+listaDeTeclas[5].onclick=tocaSomToim;
+listaDeTeclas[6].onclick=tocaSomPsh;
+listaDeTeclas[6].onclick=tocaSomPsh;
+listaDeTeclas[6].onclick=tocaSomTic;
+listaDeTeclas[6].onclick=tocaSomTom;
+
+
